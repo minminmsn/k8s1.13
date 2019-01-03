a428febfa48ba579a35f422f7f9e3eb957212089
minminmsn <minminmsn@163.com>
2019-01-03 18:50:57 +0800

    add file

---
+++ coredns/deploy.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+
+# Deploys CoreDNS to a cluster currently running Kube-DNS.
+
+set -eo pipefail
+
+show_help () {
+cat << USAGE
+usage: $0 [ -r REVERSE-CIDR ] [ -i DNS-IP ] [ -d CLUSTER-DOMAIN ] [ -t YAML-TEMPLATE ]
+    -r : Define a reverse zone for the given CIDR. You may specifcy this option more
+         than once to add multiple reverse zones. If no reverse CIDRs are defined,
+         then the default is to handle all reverse zones (i.e. in-addr.arpa and ip6.arpa)
+    -i : Specify the cluster DNS IP address. If not specificed, the IP address of
+         the existing "kube-dns" service is used, if present.
+    -s : Skips the translation of kube-dns configmap to the corresponding CoreDNS Corefile configuration.
+USAGE
+exit 0
+}
+
+# Simple Defaults
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+CLUSTER_DOMAIN=cluster.local
+YAML_TEMPLATE="$DIR/coredns.yaml.sed"
+STUBDOMAINS=""
+UPSTREAM=\\/etc\\/resolv\.conf
+FEDERATIONS=""
+
+
+# Translates the kube-dns ConfigMap to equivalent CoreDNS Configuration.
+function translate-kube-dns-configmap {
+    kube-dns-federation-to-coredns
+    kube-dns-upstreamnameserver-to-coredns
+    kube-dns-stubdomains-to-coredns
+}
+
+function kube-dns-federation-to-coredns {
+  #fed=$(kubectl -n kube-system get configmap kube-dns  -ojsonpath='{.data.federations}' 2> /dev/null | jq . | tr -d '":,')
+  if [[ ! -z ${fed} ]]; then
+  FEDERATIONS=$(sed -e '1s/^/federation /' -e 's/^/        /' -e '1i\\' <<< "${fed}") # add federation to the stanza
+  fi
+}
+
+function kube-dns-upstreamnameserver-to-coredns {
+  #up=$(kubectl -n kube-system get configmap kube-dns  -ojsonpath='{.data.upstreamNameservers}' 2> /dev/null | tr -d '[",]')
+  if [[ ! -z ${up} ]]; then
+    UPSTREAM=${up}
+  fi
+}
+
+function kube-dns-stubdomains-to-coredns {
+  STUBDOMAIN_TEMPLATE='
+    SD_DOMAIN:53 {
+      errors
+      cache 30
+      loop
+      proxy . SD_DESTINATION
+    }'
+
+  function dequote {
+    str=${1#\"} # delete leading quote
+    str=${str%\"} # delete trailing quote
+    echo ${str}
+  }
+
+  function parse_stub_domains() {
+    sd=$1
+
+  # get keys - each key is a domain
+  sd_keys=$(echo -n $sd | jq keys[])
+
+  # For each domain ...
+  for dom in $sd_keys; do
+    dst=$(echo -n $sd | jq '.['$dom'][0]') # get the destination
+
+    dom=$(dequote $dom)
+    dst=$(dequote $dst)
+
+    sd_stanza=${STUBDOMAIN_TEMPLATE/SD_DOMAIN/$dom} # replace SD_DOMAIN
+    sd_stanza=${sd_stanza/SD_DESTINATION/$dst} # replace SD_DESTINATION
+    echo "$sd_stanza"
+  done
+}
+
+  #sd=$(kubectl -n kube-system get configmap kube-dns  -ojsonpath='{.data.stubDomains}' 2> /dev/null)
+  STUBDOMAINS=$(parse_stub_domains "$sd")
+}
+
+
+# Get Opts
+while getopts "hsr:i:d:t:k:" opt; do
+    case "$opt" in
+    h)  show_help
+        ;;
+    s)  SKIP=1
+        ;;
+    r)  REVERSE_CIDRS="$REVERSE_CIDRS $OPTARG"
+        ;;
+    i)  CLUSTER_DNS_IP=$OPTARG
+        ;;
+    d)  CLUSTER_DOMAIN=$OPTARG
+        ;;
+    t)  YAML_TEMPLATE=$OPTARG
+        ;;
+    esac
+done
+
+# Conditional Defaults
+if [[ -z $REVERSE_CIDRS ]]; then
+  REVERSE_CIDRS="in-addr.arpa ip6.arpa"
+fi
+if [[ -z $CLUSTER_DNS_IP ]]; then
+  # Default IP to kube-dns IP
+  # CLUSTER_DNS_IP=$(kubectl get service --namespace kube-system kube-dns -o jsonpath="{.spec.clusterIP}")
+  CLUSTER_DNS_IP=10.254.0.10
+  if [ $? -ne 0 ]; then
+      >&2 echo "Error! The IP address for DNS service couldn't be determined automatically. Please specify the DNS-IP with the '-i' option."
+      exit 2
+  fi
+fi
+
+if [[ "${SKIP}" -ne 1 ]] ; then
+    translate-kube-dns-configmap
+fi
+
+orig=$'\n'
+replace=$'\\\n'
+sed -e "s/CLUSTER_DNS_IP/$CLUSTER_DNS_IP/g" \
+    -e "s/CLUSTER_DOMAIN/$CLUSTER_DOMAIN/g" \
+    -e "s?REVERSE_CIDRS?$REVERSE_CIDRS?g" \
+    -e "s@STUBDOMAINS@${STUBDOMAINS//$orig/$replace}@g" \
+    -e "s@FEDERATIONS@${FEDERATIONS//$orig/$replace}@g" \
+    -e "s/UPSTREAMNAMESERVER/$UPSTREAM/g" \
+    "${YAML_TEMPLATE}"
---
+++ coredns/kubernetes1.13.1集群部署coredns.md
@@ -0,0 +1,113 @@
+### kubenetes1.13.1 部署coredns
+> 参考文档
+https://www.cnblogs.com/aguncn/p/7217884.html
+https://github.com/coredns/deployment/issues/111
+https://blog.csdn.net/ccy19910925/article/details/80762025
+https://github.com/coredns/deployment/tree/master/kubernetes
+https://kubernetes.io/docs/tasks/administer-cluster/dns-custom-nameservers/#coredns
+
+
+
+### 一、修改部署文件环境变量  
+在官网下载https://github.com/coredns/deployment/tree/master/kubernetes 配置文件主要是deploy.sh和coredns.yam.sed，由于不是从kube-dns转到coredns，所以要注释掉kubectl相关操作，修改REVERSE_CIDRS、DNS_DOMAIN、CLUSTER_DNS_IP等变量为实际值，具体命令./deploy.sh -s -r 10.254.0.0/16 -i 10.254.0.10 -d clouster.local > coredns.yaml11
+```
+[root@elasticsearch01 coredns]# ./deploy.sh -h
+usage: ./deploy.sh [ -r REVERSE-CIDR ] [ -i DNS-IP ] [ -d CLUSTER-DOMAIN ] [ -t YAML-TEMPLATE ]
+    -r : Define a reverse zone for the given CIDR. You may specifcy this option more
+         than once to add multiple reverse zones. If no reverse CIDRs are defined,
+         then the default is to handle all reverse zones (i.e. in-addr.arpa and ip6.arpa)
+    -i : Specify the cluster DNS IP address. If not specificed, the IP address of
+         the existing "kube-dns" service is used, if present.
+    -s : Skips the translation of kube-dns configmap to the corresponding CoreDNS Corefile configuration.
+[root@elasticsearch01 coredns]#  ./deploy.sh -s -r 10.254.0.0/16 -i 10.254.0.10 -d cluster.local > coredns.yaml
+[root@elasticsearch01 coredns]# ls
+coredns.yaml    coredns.yaml.sed  deploy.sh
+```
+
+修改前后对比
+```
+[root@elasticsearch01 coredns]# diff coredns.yaml coredns.yaml.sed 
+58c58
+<         kubernetes cluster.local  10.254.0.0/16 {
+---
+>         kubernetes CLUSTER_DOMAIN REVERSE_CIDRS {
+62c62
+<         }
+---
+>         }FEDERATIONS
+64c64
+<         proxy . /etc/resolv.conf
+---
+>         proxy . UPSTREAMNAMESERVER
+69c69
+<     }
+---
+>     }STUBDOMAINS
+165c165
+<   clusterIP: 10.254.0.10
+---
+>   clusterIP: CLUSTER_DNS_IP
+```
+
+
+### 二、部署coredns
+```
+[root@elasticsearch01 coredns]# kubectl create -f coredns.yaml
+serviceaccount/coredns created
+clusterrole.rbac.authorization.k8s.io/system:coredns created
+clusterrolebinding.rbac.authorization.k8s.io/system:coredns created
+configmap/coredns created
+deployment.extensions/coredns created
+service/kube-dns created
+```
+
+
+### 三、修改kubelet dns服务参数并重启kubelet服务
+```
+[root@elasticsearch02 ~]# tail /k8s/kubernetes/cfg/kubelet
+--v=4 \
+--hostname-override=10.2.8.65 \
+--kubeconfig=/k8s/kubernetes/cfg/kubelet.kubeconfig \
+--bootstrap-kubeconfig=/k8s/kubernetes/cfg/bootstrap.kubeconfig \
+--config=/k8s/kubernetes/cfg/kubelet.config \
+--cert-dir=/k8s/kubernetes/ssl \
+--pod-infra-container-image=registry.cn-hangzhou.aliyuncs.com/google-containers/pause-amd64:3.0 \
+--cluster-dns=10.254.0.10 \
+--cluster-domain=cluster.local. \
+--resolv-conf=/etc/resolv.conf "
+```
+
+```
+[root@elasticsearch02 ~]# systemctl restart kubelet.service 
+[root@elasticsearch02 ~]# systemctl status kubelet.service 
+● kubelet.service - Kubernetes Kubelet
+   Loaded: loaded (/usr/lib/systemd/system/kubelet.service; enabled; vendor preset: disabled)
+   Active: active (running) since Thu 2019-01-03 16:00:20 CST; 6s ago
+ Main PID: 31924 (kubelet)
+   Memory: 80.2M
+   CGroup: /system.slice/kubelet.service
+           └─31924 /k8s/kubernetes/bin/kubelet --logtostderr=true --v=4 --hostname-override=10.2.8.65 --kubeconfig=/k8s/kubernetes...
+```
+
+
+### 四、使用busybox测试效果
+注意：拿SVC服务来测试
+```
+[root@elasticsearch01 coredns]# kubectl run -it --rm --restart=Never --image=infoblox/dnstools:latest dnstools
+If you don't see a command prompt, try pressing enter.
+dnstools# nslookup kubernetes
+Server:		10.254.0.10
+Address:	10.254.0.10#53
+
+Name:	kubernetes.default.svc.cluster.local
+Address: 10.254.0.1
+
+dnstools# nslookup nginx
+Server:		10.254.0.10
+Address:	10.254.0.10#53
+
+Name:	nginx.default.svc.cluster.local
+Address: 10.254.224.237
+```
+
+
